2deda9400fd461e902f8d5707ff6211af354239e
claudemiro <dimiro1@gmail.com>
2016-04-07 08:26:37 -0300

    Added documentation

--- darwin.go
+++ darwin.go
@@ -5,60 +5,72 @@ import (
 	"io"
 )
 
+// Migration represents a database migrations.
 type Migration struct {
 	Version     float64
 	Description string
 	Script      io.Reader
 }
 
+// Dialect is a interface used to abstract the different databases.
 type Dialect interface {
 	CreateTableSQL() string
 	MigrateSQL() string
 	LastVersionSQL() string
 }
 
+// Darwin is a helper struct to access the Validate and migratin functions
 type Darwin struct{}
 
+// Validate if the database migratins are applied and consistent
 func (d Darwin) Validate() bool {
 	return false
 }
 
+// Migrate executes the missing migrations in database
 func (d Darwin) Migrate() error {
 	return nil
 }
 
-var DefaultDarwin = Darwin{}
-
+// New returns a new Darwin struct
 func New(db *sql.DB, d Dialect, migrations []Migration) Darwin {
 	return Darwin{}
 }
 
+// NewForMySQL returns a new Darwin configured with MySQL dialect
 func NewForMySQL(db *sql.DB, migrations []Migration) Darwin {
 	return Darwin{}
 }
 
+// Validate if the database migratins are applied and consistent
 func Validate(db *sql.DB, dialect Dialect, migrations []Migration) bool {
 	return false
 }
 
+// Migrate executes the missing migrations in database
 func Migrate(db *sql.DB, dialect Dialect, migrations []Migration) error {
 	return nil
 }
 
+// MySQLDialect holds the definition of a MySQL dialect
 type MySQLDialect struct{}
 
+// CreateTableSQL returns a schema create table
 func (m MySQLDialect) CreateTableSQL() string {
 	return "CREATE TABLE schema_migrations"
 }
 
+// MigrateSQL returns a schema migrate table
 func (m MySQLDialect) MigrateSQL() string {
 	return "INSERT INTO schema_migrations"
 }
 
+// LastVersionSQL returns a new SQL fo get the last version in the database
 func (m MySQLDialect) LastVersionSQL() string {
 	return "SELECT version FROM schema_migrations ORDER BY version DESC"
 }
 
+// ByVersion implements the Sort interface sorting bt Version
 type ByVersion []Migration
 
 func (b ByVersion) Len() int           { return len(b) }
